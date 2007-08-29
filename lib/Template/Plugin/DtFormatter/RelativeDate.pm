09644fdf0f92c46c06ce02f6719be6b6d0eb20c5
Tomohiro Hosaka <bokutin@bokut.in>
2007-08-29 06:34:43 +0000

    Import of BOKUTIN/Template-Plugin-DtFormatter-RelativeDate-0.03 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-DtFormatter-RelativeDate
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BOKUTIN/Template-Plugin-DtFormatter-RelativeDate-0.03.tar.gz
    gitpan-cpan-author:       BOKUTIN
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/DtFormatter/RelativeDate.pm
+++ lib/Template/Plugin/DtFormatter/RelativeDate.pm
@@ -20,11 +20,11 @@ Template::Plugin::DtFormatter::RelativeDate - return finder like relative date.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
